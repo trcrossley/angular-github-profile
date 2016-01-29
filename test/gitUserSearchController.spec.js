7128c0a04e1afe4a3f98275cb498d44f6dc2279b
Tristan Crossley <trcrossley@gmail.com>
2016-01-29 14:27:55 +0000

    V1 Refactored

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -1,53 +1,31 @@
 describe('GitUserSearchController', function() {
+
   beforeEach(module('GitUserSearch'));
 
   var ctrl;
 
-  beforeEach(inject(function($controller) {
-    ctrl = $controller('GitUserSearchController');
-  }));
-
-  it('initialises with an empty search result and term', function() {
-    expect(ctrl.searchResult).toBeUndefined();
-    expect(ctrl.searchTerm).toBeUndefined();
-  });
-
   describe('when searching for a user', function() {
+    var fakeUserInfo, scope, fakeSearch;
 
-    var httpBackend;
-    beforeEach(inject(function($httpBackend) {
-      httpBackend = $httpBackend;
-      httpBackend
-        .expectGET("https://api.github.com/search/users")
-        .respond(
-          { items: items }
-        );
+    beforeEach(function() {
+      fakeSearch = jasmine.createSpyObj('fakeSearch', ['query']);
+
+      module({
+        Search: fakeSearch
+      });
+    });
+
+    beforeEach(inject(function ($q, $rootScope, $controller) {
+      scope = $rootScope;
+      fakeSearch.query.and.returnValue($q.when( { data: items }));
+      ctrl = $controller('GitUserSearchController');
     }));
 
-    var items = [
-      {
-        "login": "tansaku",
-        "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-        "html_url": "https://github.com/tansaku"
-      },
-      {
-        "login": "stephenlloyd",
-        "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-        "html_url": "https://github.com/stephenlloyd"
-      }
-    ];
-
-    afterEach(function() {
-      httpBackend.verifyNoOutstandingExpectation();
-      httpBackend.verifyNoOutstandingRequest();
-    });
-
-    it('displays search results', function() {
-      ctrl.searchTerm = 'hello';
+    it("includes user search results in user data", function() {
+      ctrl.searchTerm = 'tansaku';
       ctrl.doSearch();
-      httpBackend.flush();
-      expect(ctrl.searchResult.items).toEqual(items);
+      scope.$apply();
+      expect(ctrl.searchResult).toEqual(items);
     });
-
   });
 });
--- test/searchfactory.spec.js
+++ test/searchfactory.spec.js
@@ -23,28 +23,11 @@ describe('factory: Search', function() {
     httpBackend.verifyNoOutstandingRequest();
   });
 
-  var items = [
-    {
-      "login": "tansaku",
-      "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
-      "html_url": "https://github.com/tansaku"
-    },
-    {
-      "login": "stephenlloyd",
-      "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
-      "html_url": "https://github.com/stephenlloyd"
-    }
-  ];
-
-  it('responds to query', function() {
-    expect(search.query).toBeDefined();
-  });
-
   it('returns search results', function() {
     search.query('hello')
       .then(function(response) {
-        expect(response.data.items).toEqual(items)
-      })
+        expect(response.data.items).toEqual(items);
+      });
     httpBackend.flush();
   });
 });
